8a59d1a1a98901234e9293397bed4c5604982a93
Szymon Szczawinski <szymonszczawinski@gmail.com>
2023-11-05 23:29:33 +0100

    new resylt file

---
+++ coreapi/result.go
@@ -0,0 +1,25 @@
+package coreapi
+
+type Result[T any] struct {
+	data T
+	err  error
+}
+
+func NewResult[T any](data T, err error) Result[T] {
+	return Result[T]{
+		data: data,
+		err:  err,
+	}
+}
+
+func (r Result[T]) Sucess() bool {
+	return r.err != nil
+}
+
+func (r Result[T]) Error() error {
+	return r.err
+}
+
+func (r Result[T]) Data() T {
+	return r.data
+}
